ec58813ad41030b409ad776ffe2b7e0dbd8fbc20
Felipe Mattioli dos Santos <felipe.mattiolidossantos@msc.com>
2025-03-14 10:09:05 +0100

    FEAT: Adding new hangfire extension

---
+++ src/Coderaw.Settings/Extensions/Hangfire/HangfireExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Coderaw.Settings.Filters;
+using Hangfire;
+using Hangfire.Mongo;
+using Hangfire.Mongo.Migration.Strategies;
+using Hangfire.Mongo.Migration.Strategies.Backup;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace Coderaw.Settings.Extensions.Hangfire
+{
+    public static class HangFireExtensions
+    {
+        public static IServiceCollection AddHangFire(this IServiceCollection services, string connectionString, string dataBase)
+        {
+            var mongoUrlBuilder = new MongoUrlBuilder(connectionString);
+            var mongoClient = new MongoClient(mongoUrlBuilder.ToMongoUrl());
+            var mongoDataBase = dataBase;
+
+            services.AddHangfire(config =>
+            {
+                config.UseMongoStorage(mongoClient, mongoDataBase, new MongoStorageOptions
+                {
+                    CheckQueuedJobsStrategy = CheckQueuedJobsStrategy.TailNotificationsCollection,
+                    MigrationOptions = new MongoMigrationOptions
+                    {
+                        MigrationStrategy = new MigrateMongoMigrationStrategy(),
+                        BackupStrategy = new CollectionMongoBackupStrategy()
+                    },
+                    Prefix = "hangfire.mongo.metrics",
+                    CheckConnection = true,
+                });
+            });
+
+            services.AddHangfireServer();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseHangfire(this IApplicationBuilder app, string dashboardUrl)
+        {
+            app.UseHangfireDashboard($"/{dashboardUrl}", new DashboardOptions
+            {
+                Authorization = [new AllowAllAuthorizationFilter()]
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/Coderaw.Settings/Filters/AllowAllAuthorizationFilter.cs
@@ -0,0 +1,9 @@
+﻿using Hangfire.Dashboard;
+
+namespace Coderaw.Settings.Filters
+{
+    public class AllowAllAuthorizationFilter() : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context) => true;
+    }
+}
