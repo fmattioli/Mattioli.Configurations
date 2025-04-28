1502908a025b43b35aa4220b3fd6c58f564bc7f9
GabrielCodeRaw <gabriel.santana@coderaw.io>
2025-04-28 13:57:18 -0300

    feat: add class ConfigureCorsExtensions to configure cors

---
+++ src/Coderaw.Settings/Extensions/Cors/ConfigureCorsExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+
+namespace Coderaw.Settings.Extensions.Cors
+{
+    public static class ConfigureCorsExtensions
+    {
+        public static IServiceCollection AddCustomCors(this IServiceCollection services, string policyName)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(policyName, builder =>
+                {
+                    builder.SetIsOriginAllowed(origin =>
+                    {
+                        var host = new Uri(origin).Host;
+                        var ipAddresses = Dns.GetHostAddresses(host);
+                        var allowedIps = GetAllowedIps();
+
+                        return ipAddresses.Any(ip => allowedIps.Contains(ip.ToString()));
+                    })
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+                });
+            });
+
+            return services;
+        }
+
+        private static List<string> GetAllowedIps()
+        {
+            return ["85.209.93.218"];
+        }
+    }
+}
+
