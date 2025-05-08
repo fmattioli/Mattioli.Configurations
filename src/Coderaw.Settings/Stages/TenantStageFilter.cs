971e42f3393f0d04855c39df7a848914c4f7d230
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2025-05-08 15:41:44 -0300

    FEAT: Adapting tenant filter

--- src/Coderaw.Settings/Stages/TenantStageFilter.cs
+++ src/Coderaw.Settings/Stages/TenantStageFilter.cs
@@ -1,37 +1,21 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
 
+using System.Text.RegularExpressions;
+
 namespace Coderaw.Settings.Stages
 {
     public static class TenantStageFilter
     {
-        public static FilterDefinition<BsonDocument> MatchByTenant(string tenantName)
+        public static FilterDefinition<BsonDocument> MatchByTenant(IEnumerable<string> tenantNames)
         {
+            var names = tenantNames.Select(x => new BsonRegularExpression(new Regex(x, RegexOptions.IgnoreCase)));
+
             var filter = new BsonDocument(
-                "Tenant.Name",
-                new BsonDocument("$eq", tenantName));
+                "Tenant.Names",
+                new BsonDocument("$in", BsonArray.Create(names)));
 
             return new BsonDocumentFilterDefinition<BsonDocument>(filter);
-        }
-
-        public static FilterDefinition<BsonDocument> MatchByDateInterval(
-            DateTime dateIni,
-            DateTime dateEnd,
-            string fieldName)
-        {
-            if (dateIni == DateTime.MinValue || dateEnd == DateTime.MinValue)
-            {
-                return FilterDefinition<BsonDocument>.Empty;
-            }
-
-            var query = new BsonDocument(fieldName,
-                new BsonDocument
-                {
-                    { "$gte", new DateTime(dateIni.Year, dateIni.Month, dateIni.Day, 0, 0, 0, DateTimeKind.Utc) },
-                    { "$lte", new DateTime(dateEnd.Year, dateEnd.Month, dateEnd.Day, 23, 59, 59, DateTimeKind.Utc) },
-                });
-
-            return new BsonDocumentFilterDefinition<BsonDocument>(query);
         }
     }
 }
