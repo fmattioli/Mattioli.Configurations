5ad6a68b28017909ead35c2e8256ddcd3868b598
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2025-08-19 14:25:21 +0200

    FEAT: Update SerilogExtensions.cs

--- src/Mattioli.Configurations/Extensions/Loggings/SerilogExtensions.cs
+++ src/Mattioli.Configurations/Extensions/Loggings/SerilogExtensions.cs
@@ -1,36 +1,35 @@
-﻿using Mattioli.Configurations.Extensions.Loggings;
-
+using Mattioli.Configurations.Extensions.Loggings;
 using Microsoft.Extensions.Hosting;
-
 using Serilog;
 using Serilog.Events;
+using Serilog.Formatting.Json;
 using Serilog.Sinks.OpenTelemetry;
 
 namespace Mattioli.Configurations.Extensions.Loggings
 {
     public static class SerilogExtensions
     {
-        public static IHostBuilder UseSerilog(this IHostBuilder builder, string colectorUrl, string serviceName)
+        public static IHostBuilder UseSerilog(this IHostBuilder builder, string collectorUrl, string serviceName)
         {
             return builder.UseSerilog((context, loggerConfiguration) =>
             {
                 loggerConfiguration
                     .Enrich.FromLogContext()
                     .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                    .Enrich.WithProperty("ServiceName", serviceName)
                     .MinimumLevel.Debug()
                     .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                     .MinimumLevel.Override("System", LogEventLevel.Information)
                     .MinimumLevel.Override("Hangfire.Server.ServerHeartbeatProcess", LogEventLevel.Warning)
-                    .WriteTo.Console(
-                        outputTemplate:
-                        "{Timestamp:yyyy-MM-dd HH:mm:ss} | {Level} | Trace: {TraceId} | RequestPath: {RequestPath} | Env: {Environment} | {SourceContext} | {Message} | {Exception}{NewLine}")
+                    .WriteTo.Console(new JsonFormatter(renderMessage: true))
                     .WriteTo.OpenTelemetry(options =>
                     {
-                        options.Endpoint = colectorUrl;
+                        options.Endpoint = collectorUrl;
                         options.Protocol = OtlpProtocol.Grpc;
                         options.ResourceAttributes = new Dictionary<string, object>
                         {
-                            { "service.name", serviceName }
+                            { "service.name", serviceName },
+                            { "environment", context.HostingEnvironment.EnvironmentName }
                         };
                     });
             });
