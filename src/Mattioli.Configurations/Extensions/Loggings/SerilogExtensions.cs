87a4c582b6cb6bd9762f030a965d81dd2ad08332
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2025-08-19 15:22:21 -0300

    FEAT: Adding new changes related to serilog

--- src/Mattioli.Configurations/Extensions/Loggings/SerilogExtensions.cs
+++ src/Mattioli.Configurations/Extensions/Loggings/SerilogExtensions.cs
@@ -2,7 +2,6 @@ using Mattioli.Configurations.Extensions.Loggings;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
-using Serilog.Formatting.Json;
 using Serilog.Sinks.OpenTelemetry;
 
 namespace Mattioli.Configurations.Extensions.Loggings
@@ -15,23 +14,36 @@ namespace Mattioli.Configurations.Extensions.Loggings
             {
                 loggerConfiguration
                     .Enrich.FromLogContext()
-                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
-                    .Enrich.WithProperty("ServiceName", serviceName)
-                    .MinimumLevel.Debug()
-                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                    .MinimumLevel.Override("System", LogEventLevel.Information)
-                    .MinimumLevel.Override("Hangfire.Server.ServerHeartbeatProcess", LogEventLevel.Warning)
-                    .WriteTo.Console(new JsonFormatter(renderMessage: true))
-                    .WriteTo.OpenTelemetry(options =>
-                    {
-                        options.Endpoint = collectorUrl;
-                        options.Protocol = OtlpProtocol.Grpc;
-                        options.ResourceAttributes = new Dictionary<string, object>
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName);
+
+                if (context.HostingEnvironment.IsDevelopment())
+                {
+                    loggerConfiguration
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                        .MinimumLevel.Override("System", LogEventLevel.Information)
+                        .WriteTo.Console(outputTemplate:
+                            "{Timestamp:yyyy-MM-dd HH:mm:ss} | {Level} | Trace: {TraceId} | RequestPath: {RequestPath} | Env: {Environment} | {SourceContext} | {Message} | {Exception}{NewLine}");
+                }
+                else
+                {
+                    loggerConfiguration
+                        .MinimumLevel.Information()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                        .MinimumLevel.Override("System", LogEventLevel.Warning)
+                        .MinimumLevel.Override("Hangfire.Server.ServerHeartbeatProcess", LogEventLevel.Warning)
+                        .WriteTo.Console(outputTemplate:
+                            "{Timestamp:yyyy-MM-dd HH:mm:ss} | {Level} | {Message} | {Exception}{NewLine}")
+                        .WriteTo.OpenTelemetry(options =>
                         {
-                            { "service.name", serviceName },
-                            { "environment", context.HostingEnvironment.EnvironmentName }
-                        };
-                    });
+                            options.Endpoint = collectorUrl;
+                            options.Protocol = OtlpProtocol.Grpc;
+                            options.ResourceAttributes = new Dictionary<string, object>
+                            {
+                                { "serviceName", serviceName }
+                            };
+                        });
+                }
             });
         }
     }
