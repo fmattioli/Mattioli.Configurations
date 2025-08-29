762c3c061d26d897cada18da93b2230fa7ab031c
Felipe Mattioli dos Santos <felipe.mattiolidossantos@msc.com>
2025-08-29 11:20:19 +0200

    FEAT: Changing log and adding new middleware

--- src/Mattioli.Configurations/Extensions/Loggings/SerilogExtensions.cs
+++ src/Mattioli.Configurations/Extensions/Loggings/SerilogExtensions.cs
@@ -1,34 +1,47 @@
 using Mattioli.Configurations.Extensions.Loggings;
+using Mattioli.Configurations.Middlewares;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Events;
+using Serilog.Exceptions;
 using Serilog.Sinks.OpenTelemetry;
 
 namespace Mattioli.Configurations.Extensions.Loggings
 {
     public static class SerilogExtensions
     {
+        public static IApplicationBuilder UseRequestContextLogging(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RequestContextLoggingMiddleware>();
+            app.UseSerilogRequestLogging();
+
+            return app;
+        }
+
         public static IHostBuilder UseSerilog(this IHostBuilder builder, string collectorUrl, string serviceName)
         {
             return builder.UseSerilog((context, loggerConfiguration) =>
             {
                 loggerConfiguration
+                    .ReadFrom.Configuration(context.Configuration)
                     .Enrich.FromLogContext()
-                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName);
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                    .Enrich.WithProperty("ServiceName", serviceName)
+                    .Enrich.WithExceptionDetails();
 
                 if (context.HostingEnvironment.IsDevelopment())
                 {
                     loggerConfiguration
                         .MinimumLevel.Debug()
                         .WriteTo.Console(outputTemplate:
-                            "{Timestamp:yyyy-MM-dd HH:mm:ss} | {Level} | Trace: {TraceId} | RequestPath: {RequestPath} | Env: {Environment} | {SourceContext} | {Message} | {Exception}{NewLine}");
+                            "{Timestamp:yyyy-MM-dd HH:mm:ss} | Level {Level} | CorrelationId: {CorrelationId} | RequestPath: {RequestPath} | Env: {Environment} | {SourceContext} | {Message} | {Exception}{NewLine}");
                 }
                 else
                 {
                     loggerConfiguration
-                        .MinimumLevel.Information()                        
+                        .MinimumLevel.Information()
                         .WriteTo.Console(outputTemplate:
-                            "{Timestamp:yyyy-MM-dd HH:mm:ss} | {Level} | Trace: {TraceId} | RequestPath: {RequestPath} | Env: {Environment} | {SourceContext} | {Message} | {Exception}{NewLine}")
+                            "{Timestamp:yyyy-MM-dd HH:mm:ss} | {Level} | CorrelationId: {CorrelationId} Trace: {TraceId} | RequestPath: {RequestPath} | Env: {Environment} | {SourceContext} | {Message} | {Exception}{NewLine}")
                         .WriteTo.OpenTelemetry(options =>
                         {
                             options.Endpoint = collectorUrl;
