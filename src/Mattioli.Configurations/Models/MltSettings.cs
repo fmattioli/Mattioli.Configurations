41f76aa67e08bf7978f8d65d10cfe698fc3589f1
Guilherme Giannini <guilherme.giannini@smartinnovate.com.br>
2025-10-14 11:01:25 -0300

    adicionando required a outras proprieddes do Mlt

--- src/Mattioli.Configurations/Models/MltSettings.cs
+++ src/Mattioli.Configurations/Models/MltSettings.cs
@@ -2,12 +2,12 @@
 {
     public class MltSettings
     {
-        public string OpenTelemetryColectorUrl { get; set; }
-        public string ApplicationName { get; set; }
+        public required string OpenTelemetryColectorUrl { get; set; }
+        public required string ApplicationName { get; set; }
         public required string Dsn { get; set; }
-        public double TracesSampleRate { get; set; }
-        public bool AttachStacktrace { get; set; }
-        public string DiagnosticLevel { get; set; }
+        public required double TracesSampleRate { get; set; }
+        public required bool AttachStacktrace { get; set; }
+        public required string DiagnosticLevel { get; set; }
         public required string Url { get; set; }
     }
 }
