a0ce1baec4a121ac4f7583da96a051ee66c4ceed
Miguel Totti <migueltotti2005@gmail.com>
2025-11-28 09:25:01 -0300

    feat: add UseMiddleware extension method to CorrelationIdEnricherMiddleware

---
+++ src/Mattioli.Configurations/Extensions/Middlewares/MiddlewaresExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Mattioli.Configurations.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace Mattioli.Configurations.Extensions.Middlewares;
+
+public static class MiddlewaresExtensions
+{
+    public static IApplicationBuilder UseCorrelationIdEnricherMiddleware(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<CorrelationIdEnricherMiddleware>();
+
+        return app;
+    }
+}
